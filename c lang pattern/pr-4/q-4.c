9af898977ff2c64d3ac8a8625066d356148945a0
ayushigorasiya <ayushigorasiya287@gmail.com>
2024-06-24 17:13:29 +0530

    Add files via upload

---
+++ c lang pattern/pr-4/q-4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main(){
+	
+	int i , j , k=11;
+	printf("\n");
+	for(i=1; i<=4; i++){
+		
+			for(j=1; j<=i; j++){
+				printf("%d " , k);
+				k++;
+			}
+			
+		printf("\n");
+	}
+}
